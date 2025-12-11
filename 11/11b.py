eae43cde8fdfc540e42970753f944d2a9e1c9ea4
gifflarn <lukas.brandtbrune@gmail.com>
2025-12-12 00:10:58 +0100

    reduce solution

--- 11/11b.py
+++ 11/11b.py
@@ -5,66 +5,34 @@ t0 = time.time()
 with open("input.txt", "r") as f:
     input_lines = f.readlines()
 
-class Node:
-    def __init__(self,val):
-        self.val = val
-        self.next_nodes = []
-    
-    def link(self, N):
-        self.next_nodes.append(N)
-
-    def get_paths(self):
-        return self.next_nodes
-
-    
-class UniqueChain:
-    def __init__(self):
-        self.nodes = {} 
-
-    def get_or_create_node(self, val):
-        if val not in self.nodes:
-            new_node = Node(val)
-            self.nodes[val] = new_node
-        return self.nodes[val]
-
-    def link_nodes(self, val_a, val_b):
-        node_a = self.get_or_create_node(val_a)
-        node_b = self.get_or_create_node(val_b)
-        
-        node_a.link(node_b)
-
-UC = UniqueChain()
 start_node = None
+graph = {}
 for input_row in input_lines:
     source, paths = input_row.strip().split(":")
-    source_node = UC.get_or_create_node(source)
-    if source == "svr":
-        start_node = source_node
-    [UC.link_nodes(source, path) for path in paths.split()]
+    graph[source] = paths.split()
 
 solved_nodes = dict()
 
-def solve(start_node, chain):
-    def dfs(node_val, have_fft, have_dac):
-        input_data = (node_val, have_fft, have_dac)
-        if input_data in solved_nodes:
-            return solved_nodes[input_data]
-        if node_val == "fft":
-            have_fft = True
-        if node_val == "dac":
-            have_dac = True
-        if node_val == "out":
-            return 1 if (have_fft and have_dac) else 0
-        node = chain.nodes[node_val]
-        total = 0
-        for nxt in node.get_paths():
-            total += dfs(nxt.val, have_fft, have_dac)
-        solved_nodes[input_data] = total
-        return total
+def traverse(node_val, have_fft, have_dac):
+    input_data = (node_val, have_fft, have_dac)
+    if input_data in solved_nodes:
+        return solved_nodes[input_data]
+    if node_val == "fft":
+        have_fft = True
+    if node_val == "dac":
+        have_dac = True
+    if node_val == "out":
+        result = 1 if (have_fft and have_dac) else 0
+        solved_nodes[input_data] = result
+        return result
+    total = 0
+    for nxt in graph.get(node_val, []):
+        total += traverse(nxt, have_fft, have_dac)
+    solved_nodes[input_data] = total
+    return total
 
-    return dfs(start_node.val, False, False)
     
-print(solve(start_node, UC))
+print(traverse("svr", False, False))
 
 t1 = time.time()
 print("executed in {0:0.1f}ms".format((t1-t0)*1000))
